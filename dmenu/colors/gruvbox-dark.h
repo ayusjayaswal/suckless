4b746f425670be3e3dd997d33fad7b8bea70197a
AyushJayaswal <95228530+Jayaswal-Ayush@users.noreply.github.com>
2022-02-20 05:47:52 +0530

    update

---
+++ dmenu/colors/gruvbox-dark.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeSel] = { "#000000", "#83a598" },
+	[SchemeSelHighlight] = { "#3c3836", "#83a598" },
+	[SchemeNormHighlight] = { "#fabd2f", "#282828" },
+	[SchemeOut] = { "#fabd2f", "#1d2021" },
+	[SchemeMid] = { "#ebdbb2", "#1c1f24" },
+};
