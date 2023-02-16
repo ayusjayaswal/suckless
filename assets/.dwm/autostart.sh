40fca6c39da2a800be3e026d0a2c50fec8c0354a
Ayush Jayaswal <95228530+ayusjayaswal@users.noreply.github.com>
2023-02-16 13:26:07 +0530

    autostart

--- assets/.dwm/autostart.sh
+++ assets/.dwm/autostart.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
+bgdir=(~/.wallpapers/*) &&
+wallpaper=$(printf "%s\n" "${bgdir[RANDOM % ${#bgdir[@]}]}") &&
+xwallpaper --zoom $wallpaper &&
 picom &
-feh --bg-fill --randomize ~/.dwm/backgr/* &
+sxhkd &
+mpd &
+slstatus
+
 #dwmblocks &
-while true; do
-        xsetroot -name " $(free -m | grep '^Mem' | awk '{print "  "$3"/"$2""}' | lolcat )   $(date +"  %I:%M     %d %b %Y")"
-        sleep 2
-done &
+# while true; do
+#         xsetroot -name " $(free -m | grep '^Mem' | awk '{print "  "$3"/"$2""}' | lolcat )   $(date +"  %I:%M     %d %b %Y")"
+#         sleep 2
+# done &
 
